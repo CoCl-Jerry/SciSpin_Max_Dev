739f0e45801571d356c7e2011e7cd89ae533519f
Jerry <jerry@cosecloud.com>
2020-03-20 10:54:13 -0500

    Update UI_Update.py

--- _python/UI_Update.py
+++ _python/UI_Update.py
@@ -77,7 +77,7 @@ def sensor_update(self):
 
 def temp_update(self):
     self.temp_label.setText("Temperature: " + str(Settings.temperature))
-    Commands.fan_update():
+    Commands.fan_update()
 
 
 def LED_validate(self):
