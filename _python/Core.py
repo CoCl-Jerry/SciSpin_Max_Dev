80f5aeacb6e0232b5649cff38492f6377b45f865
Jerry <jerry@cosecloud.com>
2020-07-31 04:29:34 -0500

    Update Core.py

--- _python/Core.py
+++ _python/Core.py
@@ -13,32 +13,35 @@ while True:
     connection, client_address = sock.accept()
 
     while True:
-        recieved = connection.recv(1024).decode("utf-8")
-        print(recieved)
+        try:
+            recieved = connection.recv(1024).decode("utf-8")
+            print(recieved)
 
-        CMD = recieved.split('~', 9)
+            CMD = recieved.split('~', 9)
 
-        if(CMD[0] == 'A'):
-            with PiCamera() as camera:
-                camera.resolution = (int(CMD[1]), int(CMD[2]))
-                camera._set_rotation(90 * int(CMD[3]))
-                camera.zoom = (
-                    int(CMD[4]) / 100, int(CMD[5]) / 100, int(CMD[6]) / 100, int(CMD[7]) / 100)
-                sleep(2)
-                if(int(CMD[8]) == 1):
-                    camera.capture("out.jpg")
+            if(CMD[0] == 'A'):
+                with PiCamera() as camera:
+                    camera.resolution = (int(CMD[1]), int(CMD[2]))
+                    camera._set_rotation(90 * int(CMD[3]))
+                    camera.zoom = (
+                        int(CMD[4]) / 100, int(CMD[5]) / 100, int(CMD[6]) / 100, int(CMD[7]) / 100)
+                    sleep(2)
+                    if(int(CMD[8]) == 1):
+                        camera.capture("out.jpg")
+                    else:
+                        camera.capture("out.png")
+
+                if(int(CMD[4]) == 1):
+                    f = open("out.jpg", "rb")
                 else:
-                    camera.capture("out.png")
-
-            if(int(CMD[8]) == 1):
-                f = open("out.jpg", "rb")
-            else:
-                f = open("out.png", "rb")
-            l = f.read(512)
-            while (l):
-                connection.send(l)
+                    f = open("out.png", "rb")
                 l = f.read(512)
-            f.close()
-            break
+                while (l):
+                    connection.send(l)
+                    l = f.read(512)
+                f.close()
+                break
+        except Exception as e:
+            print(e)
 
     connection.close()
