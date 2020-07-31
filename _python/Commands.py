6492c82b590acfac4c68f9e539976c1c44c22b93
Jerry <jerry@cosecloud.com>
2020-07-31 02:17:06 -0500

    sa

--- _python/Commands.py
+++ _python/Commands.py
@@ -119,12 +119,7 @@ def frame_slider_change(self):
 
 def core_slider_change(self):
     Settings.core_RPM = self.core_verticalSlider.sliderPosition() / 10
-    self.core_spinBox.blockSignals(True)
     self.core_spinBox.setValue(Settings.core_RPM)
-    CMD = "2~" + str(Settings.core_RPM * 10)
-    Settings.sendCMD(Settings.core_addr, CMD)
-
-    self.core_spinBox.blockSignals(False)
 
 
 def linked_slider_change(self):
@@ -146,9 +141,9 @@ def linked_slider_change(self):
         self.core_spinBox.setValue(Settings.core_RPM)
         self.frame_spinBox.setValue(Settings.frame_RPM)
 
-    CMD = "2~" + str(Settings.frame_RPM * 10)
+    CMD = "2~" + str(int(Settings.frame_RPM * 10))
     Settings.sendCMD(Settings.frame_addr, CMD)
-    CMD = "2~" + str(Settings.core_RPM * 10)
+    CMD = "2~" + str(int(Settings.core_RPM * 10))
     Settings.sendCMD(Settings.core_addr, CMD)
 
     self.core_spinBox.blockSignals(False)
--- _python/Functions.py
+++ _python/Functions.py
@@ -98,39 +98,3 @@ def core_spin_select(self):
         Commands.linked_spin_change(self)
     else:
         Commands.core_spin_select(self)
-
-
-def reverse_frame_select(self):
-    if(Settings.LINKED):
-        if(Settings.core_dir == 0):
-            self.coreReverse_pushButton.setIcon(Settings.reverse)
-            self.frameReverse_pushButton.setIcon(Settings.reverse)
-        else:
-            self.coreReverse_pushButton.setIcon(Settings.forward)
-            self.frameReverse_pushButton.setIcon(Settings.forward)
-        Commands.reverse_core_select(self)
-        Commands.reverse_frame_select(self)
-    else:
-        if(Settings.frame_dir == 0):
-            self.frameReverse_pushButton.setIcon(Settings.reverse)
-        else:
-            self.frameReverse_pushButton.setIcon(Settings.forward)
-        Commands.reverse_frame_select(self)
-
-
-def reverse_core_select(self):
-    if(Settings.LINKED):
-        if(Settings.core_dir == 0):
-            self.coreReverse_pushButton.setIcon(Settings.reverse)
-            self.frameReverse_pushButton.setIcon(Settings.reverse)
-        else:
-            self.coreReverse_pushButton.setIcon(Settings.forward)
-            self.frameReverse_pushButton.setIcon(Settings.forward)
-        Commands.reverse_core_select(self)
-        Commands.reverse_frame_select(self)
-    else:
-        if(Settings.core_dir == 0):
-            self.coreReverse_pushButton.setIcon(Settings.reverse)
-        else:
-            self.coreReverse_pushButton.setIcon(Settings.forward)
-        Commands.reverse_core_select(self)
