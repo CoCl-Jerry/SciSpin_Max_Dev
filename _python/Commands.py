114749ed52d9aae93775e96f9835692aac166965
Jerry <jerry@cosecloud.com>
2020-09-26 02:47:26 -0500

    Update Commands.py

--- _python/Commands.py
+++ _python/Commands.py
@@ -130,7 +130,7 @@ def spin_change(mot, self):
     self.frame_verticalSlider.blockSignals(False)
 
     CMD = "1~2~" + getMicrostep(Settings.frame_RPM * 100) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
-        Settings.frame_RPM)) * 100)]) + "~" + getMicrostep(Settings.core_RPM * 20) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
+        Settings.frame_RPM)) * 100)]) + "~" + getMicrostep(Settings.core_RPM * 100) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
             Settings.core_RPM)) * 100)])
     Settings.sendCMD(CMD)
 
@@ -173,7 +173,7 @@ def slider_change(mot, self):
     self.frame_verticalSlider.blockSignals(False)
 
     CMD = "1~2~" + getMicrostep(Settings.frame_RPM * 100) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
-        Settings.frame_RPM)) * 100)]) + "~" + getMicrostep(Settings.core_RPM * 20) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
+        Settings.frame_RPM)) * 100)]) + "~" + getMicrostep(Settings.core_RPM * 100) + "~" + str(Settings.speed_dict[int(decimal.Decimal(str(
             Settings.core_RPM)) * 100)])
     Settings.sendCMD(CMD)
 
