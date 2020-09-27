2f03f54d4b21a44d4337f8c0500af2b091b4c579
Jerry <jerry@cosecloud.com>
2020-09-27 15:22:58 -0500

    Update Settings.py

--- _python/Settings.py
+++ _python/Settings.py
@@ -39,10 +39,10 @@ def init():
     core_RPM = 0.3
 
     global frame_enabled
-    frame_enabled = False
+    frame_enabled = True
 
     global core_enabled
-    core_enabled = False
+    core_enabled = True
 
     global frame_microstep
     frame_microstep = 256
