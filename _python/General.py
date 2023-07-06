d014d1f5439a79f7fb4512f3ef8824a2f987bfdd
jerry@hercules <jerry@cosecloud.com>
2023-07-06 14:29:13 -0500

    update

--- _python/General.py
+++ _python/General.py
@@ -17,31 +17,23 @@ palette_green.setColor(QPalette.WindowText, QColor(0, 255, 0))
 global camera_error_image
 camera_error_image = QImage("../_image/camera_error.png")
 
-
-def init():
-    pass
-
 # ---------------------------------------------------------------------------- #
 #                      start of system status declarations                     #
 # ---------------------------------------------------------------------------- #
+global camera_status
+camera_status = False
 
+global motor_status
+motor_status = False
 
-def setup():
-    global camera_status
-    camera_status = False
+global sensor_status
+sensor_status = False
 
-    global motor_status
-    motor_status = False
+global acc_status
+acc_status = False
 
-    global sensor_status
-    sensor_status = False
-
-    global acc_status
-    acc_status = False
-
-    global storage_remaining
-    storage_remaining = "N/A"
-
+global storage_remaining
+storage_remaining = "N/A"
 
 # ---------------------------------------------------------------------------- #
 #                         start of general declarations                        #
--- _python/Main.py
+++ _python/Main.py
@@ -20,7 +20,12 @@ class MainWindow(QMainWindow, Clinostat_UI.Ui_MainWindow):
         super(self.__class__, self).__init__()
         self.setupUi(self)
 
-#         Settings.init()
+# ---------------------------------------------------------------------------- #
+#                                 initialzation                                #
+# ---------------------------------------------------------------------------- #
+
+
+        Settings.init()
 #         Call_Thread.sensor_init(self)
 #         Commands.init()
 
